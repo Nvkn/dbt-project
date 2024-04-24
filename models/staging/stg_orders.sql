ea583cfc63a70d300f70571c19e3b3ef3233c8a8
Kaja Gec <dbtprueba2@gmail.com>
2024-04-24 15:00:28 +0000

    Creating RAW orders and costumers tables reading from sample data

---
+++ models/staging/stg_orders.sql
@@ -0,0 +1,26 @@
+-- models/staging/stg_orders.sql
+
+{{ config(
+    materialized='incremental',
+    unique_key='O_ORDERKEY'
+) }}
+
+WITH new_orders AS (
+    SELECT
+        O_ORDERKEY,
+        O_CUSTKEY,
+        O_ORDERSTATUS,
+        O_TOTALPRICE,
+        O_ORDERDATE,
+        O_ORDERPRIORITY,
+        O_CLERK
+    FROM {{ source('tpch_sf1', 'orders') }}
+    {% if is_incremental() %}
+    WHERE O_ORDERKEY > (SELECT COALESCE(MAX(O_ORDERKEY), 0) FROM {{ this }})
+    {% endif %}
+)
+
+SELECT *
+FROM new_orders
+ORDER BY O_ORDERKEY
+LIMIT 10
