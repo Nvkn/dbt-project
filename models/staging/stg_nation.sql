1b9faefeb1c840ab231901386ca15de264662ae7
Cate Pib <catepib713@agaseo.com>
2024-05-02 11:16:08 +0000

    timezone

--- models/staging/stg_nation.sql
+++ models/staging/stg_nation.sql
@@ -11,7 +11,7 @@ WITH source_data AS (
         N_NAME,
         N_REGIONKEY,
         cast(CONV_USD_TO_LOCAL AS FLOAT) AS N_USD_TO_LOCAL,
-        CONV_UTC_DIFFERENCE AS N_UTC_DIFF
+        CONV_TIMEZONE AS N_TIMEZONE
     FROM {{ source('db_source', 'nation') }}
     JOIN {{ source('db_source', 'conversion') }} ON N_NATIONKEY = CONV_NATIONKEY
 )
