d9da49677fe60eadef8ad78cbf98af5740223fa2
Kaja Gec <dbtprueba2@gmail.com>
2024-05-02 08:56:37 +0000

    sales/return column implemented in fact orders

--- models/staging/stg_nation.sql
+++ models/staging/stg_nation.sql
@@ -12,7 +12,7 @@ WITH source_data AS (
         N_REGIONKEY,
         cast(CONV_USD_TO_LOCAL AS FLOAT) AS N_USD_TO_LOCAL
     FROM {{ source('db_source', 'nation') }}
-    JOIN {{ source('db_source', 'currency_conversion') }} ON N_NATIONKEY = CONV_NATIONKEY
+    JOIN {{ source('db_source', 'conversion') }} ON N_NATIONKEY = CONV_NATIONKEY
 )
 
 SELECT *
