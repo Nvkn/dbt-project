f43747d678186ae5d1a222a5f970ed96b557afcd
Kaja Gec <dbtprueba2@gmail.com>
2024-05-02 11:46:54 +0000

    adding times for customer and store

--- models/intermediate/dimensions/dim_time.sql
+++ models/intermediate/dimensions/dim_time.sql
@@ -23,12 +23,72 @@ WITH date_elements AS (
             ELSE 'Second Half'
         END AS T_Calendar_Half_Year,
         TO_CHAR(O_ORDERDATE, 'YYYY') AS T_Calendar_Year,
-        CASE 
+        CASE
             WHEN T_Day_Number_of_Week IN (1, 5) THEN 'Weekday'
             ELSE 'Weekend'
         END AS T_Weekday_Indicator
     FROM {{ ref('stg_orders') }}
 )
+, date_elements_customer AS (
+    SELECT DISTINCT
+        TO_CHAR(CONVERT_TIMEZONE('UTC', N_TIMEZONE, O_ORDERDATE), 'YYYYMMDDHH24MISS')::INT AS T_Timekey,
+        CONVERT_TIMEZONE('UTC', N_TIMEZONE, O_ORDERDATE) AS T_SQL_Date_Stamp,
+        DATE(CONVERT_TIMEZONE('UTC', N_TIMEZONE, O_ORDERDATE)) AS T_Date,
+        EXTRACT(dayofweek FROM CONVERT_TIMEZONE('UTC', N_TIMEZONE, O_ORDERDATE)) AS T_Day_Number_of_Week,
+        EXTRACT(day FROM CONVERT_TIMEZONE('UTC', N_TIMEZONE, O_ORDERDATE)) AS T_Day_Number_of_Month,
+        EXTRACT(dayofyear FROM CONVERT_TIMEZONE('UTC', N_TIMEZONE, O_ORDERDATE)) AS T_Day_Number_of_Year,
+        EXTRACT(weekofyear FROM CONVERT_TIMEZONE('UTC', N_TIMEZONE, O_ORDERDATE)) AS T_Calendar_Week_Number_in_Year,
+        MONTHNAME(CONVERT_TIMEZONE('UTC', N_TIMEZONE, O_ORDERDATE)) AS T_Calendar_Month_Name,
+        MONTH(CONVERT_TIMEZONE('UTC', N_TIMEZONE, O_ORDERDATE)) AS T_Calendar_Month_Number_In_Year,
+        TO_CHAR(CONVERT_TIMEZONE('UTC', N_TIMEZONE, O_ORDERDATE), 'YYYY-MM') AS T_Calendar_Year_Month,
+        DATE_PART(quarter, CONVERT_TIMEZONE('UTC', N_TIMEZONE, O_ORDERDATE)) AS T_Calendar_Quarter,
+        CASE
+            WHEN  T_Calendar_Quarter IN (1, 2) THEN 'First Half'
+            ELSE 'Second Half'
+        END AS T_Calendar_Half_Year,
+        TO_CHAR(CONVERT_TIMEZONE('UTC', N_TIMEZONE, O_ORDERDATE), 'YYYY') AS T_Calendar_Year,
+        CASE
+            WHEN T_Day_Number_of_Week IN (1, 5) THEN 'Weekday'
+            ELSE 'Weekend'
+        END AS T_Weekday_Indicator
+    FROM {{ ref('stg_orders') }}
+    JOIN {{ ref('stg_customer') }} ON O_CUSTKEY = C_CUSTKEY
+    JOIN {{ ref('stg_nation') }} ON C_NATIONKEY = N_NATIONKEY
+    WHERE T_Timekey
+    NOT IN ( select T_Timekey from date_elements )
+)
+, date_elements_store AS (
+    SELECT DISTINCT
+        TO_CHAR(CONVERT_TIMEZONE('UTC', N_TIMEZONE, O_ORDERDATE), 'YYYYMMDDHH24MISS')::INT AS T_Timekey,
+        CONVERT_TIMEZONE('UTC', N_TIMEZONE, O_ORDERDATE) AS T_SQL_Date_Stamp,
+        DATE(CONVERT_TIMEZONE('UTC', N_TIMEZONE, O_ORDERDATE)) AS T_Date,
+        EXTRACT(dayofweek FROM CONVERT_TIMEZONE('UTC', N_TIMEZONE, O_ORDERDATE)) AS T_Day_Number_of_Week,
+        EXTRACT(day FROM CONVERT_TIMEZONE('UTC', N_TIMEZONE, O_ORDERDATE)) AS T_Day_Number_of_Month,
+        EXTRACT(dayofyear FROM CONVERT_TIMEZONE('UTC', N_TIMEZONE, O_ORDERDATE)) AS T_Day_Number_of_Year,
+        EXTRACT(weekofyear FROM CONVERT_TIMEZONE('UTC', N_TIMEZONE, O_ORDERDATE)) AS T_Calendar_Week_Number_in_Year,
+        MONTHNAME(CONVERT_TIMEZONE('UTC', N_TIMEZONE, O_ORDERDATE)) AS T_Calendar_Month_Name,
+        MONTH(CONVERT_TIMEZONE('UTC', N_TIMEZONE, O_ORDERDATE)) AS T_Calendar_Month_Number_In_Year,
+        TO_CHAR(CONVERT_TIMEZONE('UTC', N_TIMEZONE, O_ORDERDATE), 'YYYY-MM') AS T_Calendar_Year_Month,
+        DATE_PART(quarter, CONVERT_TIMEZONE('UTC', N_TIMEZONE, O_ORDERDATE)) AS T_Calendar_Quarter,
+        CASE
+            WHEN  T_Calendar_Quarter IN (1, 2) THEN 'First Half'
+            ELSE 'Second Half'
+        END AS T_Calendar_Half_Year,
+        TO_CHAR(CONVERT_TIMEZONE('UTC', N_TIMEZONE, O_ORDERDATE), 'YYYY') AS T_Calendar_Year,
+        CASE
+            WHEN T_Day_Number_of_Week IN (1, 5) THEN 'Weekday'
+            ELSE 'Weekend'
+        END AS T_Weekday_Indicator
+    FROM {{ ref('stg_orders') }}
+    JOIN {{ ref('stg_store') }} ON O_STOREKEY = ST_STOREKEY
+    JOIN {{ ref('stg_nation') }} ON ST_NATIONKEY = N_NATIONKEY
+    WHERE T_TIMEKEY
+    NOT IN (SELECT T_TIMEKEY FROM date_elements UNION SELECT T_TIMEKEY FROM date_elements_customer)
+)
 
 SELECT *
 FROM date_elements
+UNION
+SELECT * FROM date_elements_customer
+UNION
+SELECT * FROM date_elements_store
